e416c09e1a26dfe2a0b37c80d6703b2d36acb6c0
Agathangelidis Grigorios <gregory82gr@gmail.com>
2023-05-14 13:34:11 +0300

    minor fixes

--- EFCoreCodeFirstSample/Controllers/WeatherForecastController.cs
+++ EFCoreCodeFirstSample/Controllers/WeatherForecastController.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EFCoreCodeFirstSample.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -23,7 +23,7 @@ namespace EFCoreCodeFirstSample.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
